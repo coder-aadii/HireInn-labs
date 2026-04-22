7981749989bade2081f0a4e14bd27fba5098b433
coder-aadii <aditya.aerpule191996@gmail.com>
2026-04-22 17:38:16 +0530

    Improve job show page presentation

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,9 @@ Rails.application.config.assets.version = "1.0"
 
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/intl-tel-input/dist/css")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/intl-tel-input/dist/js")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/intl-tel-input/dist/img")
 Rails.application.config.assets.paths << Rails.root.join("app/assets/fonts")
 Rails.application.config.assets.precompile << "bootstrap.bundle.min.js"
-Rails.application.config.assets.precompile += %w[*.ttf *.woff *.woff2 *.otf]
+Rails.application.config.assets.precompile += %w[intlTelInput-no-assets.css intlTelInputWithUtils.min.js *.ttf *.woff *.woff2 *.otf *.png *.webp]
